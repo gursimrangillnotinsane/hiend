659d404daac8475ae36565f14cdce3691413f468
gursimrangillnotinsane <132660017+gursimrangillnotinsane@users.noreply.github.com>
2024-12-13 15:10:45 -0500

    margin

--- app/contactus/page.tsx
+++ app/contactus/page.tsx
@@ -121,7 +121,7 @@ const ContactHomePage = () => {
 
 
             </section >
-            <section className="section">
+            <section className="section mt-40">
                 <h3 className="h3 title lg:title-60 title-130  text-center py-5 headline-outline">Get In Touch</h3>
                 <ContactForm color={'white'} />
             </section>
